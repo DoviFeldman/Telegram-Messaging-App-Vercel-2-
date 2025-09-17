c232c9d074454edfbe70f15dccb3f806a29fca30
DoviFeldman <96088906+DoviFeldman@users.noreply.github.com>
2025-09-17 16:57:33 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,21 @@
+// api/index.js - Main Express.js server for Vercel
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
 
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public')); // For serving the HTML file
+
+// Serve the HTML file at root
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// Import API routes
+app.use('/api', require('./telegram'));
+
+module.exports = app;
